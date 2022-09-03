a04b87042e1270dbee59a08faf3675dd00177cbb
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-09-04 03:25:19 +0600

    footer added, dashboard and home page populated

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [SidebarComponent, HeaderComponent],
+  declarations: [SidebarComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, AngularMaterialModule, RouterModule],
-  exports: [SidebarComponent, HeaderComponent],
+  exports: [SidebarComponent, HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
