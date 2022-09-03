02923c29a07098dce9ddf0abbc0d9b4aef41329d
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-09-04 02:34:27 +0600

    header and sidenav styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  isSideNavOppened: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  sidebarToggler(){
+    this.isSideNavOppened = !this.isSideNavOppened;
+  }
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SidebarComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AngularMaterialModule, RouterModule],
   exports: [SidebarComponent, HeaderComponent],
 })
 export class LayoutModule {}
